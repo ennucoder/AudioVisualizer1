931ff0402f28d520b91e150e1538639c2a45be86
ennucoder <93170783+ennucoder@users.noreply.github.com>
2021-12-14 20:26:31 +0900

    Day4
    ~41:08

--- visualizer.js
+++ visualizer.js
@@ -5,23 +5,33 @@ function main(){
     canvas.height = window.innerHeight;
 
     class Bar{
-        constructor(x, y, width, height, color){
+        constructor(x, y, width, height, color,index){
+
             this.x =x;
             this.y =y;
             this.width =width;
             this.height =height;
             this.color =color;
+            this.index = index;
     
         }
         update(micInput){
-            this.height = micInput;
+            this.height = micInput * 1000;
             
             
         }
         draw(context){
-            context.fillStyle =this.color;
-            context.fillRect(this.x, this.y, this.width, this.height);
+            context.strokeStyle =this.color;
+            context.save();
 
+            context.translate(canvas.width/2, canvas.height/2);
+            context.rotate(this.index);
+    
+            context.beginPath();
+            context.moveTo(0, 0);
+            context.lineTo(0, this.height);
+            context.stroke();
+            context.restore();
     
         }
     }
@@ -33,7 +43,7 @@ function main(){
     function createBars(){
         for(let i = 0; i < 256; i++){
             let color = 'hsl(' + i * 2 + ', 100%, 50%)';
-            bars.push(new Bar(i * barWidth, canvas.height/2, 1, 20, color));
+            bars.push(new Bar(i * barWidth, canvas.height/2, 1, 20, color,i));
 
             
         }
@@ -42,13 +52,20 @@ function main(){
     createBars();
     console.log(bars);
     function animate(){
-        ctx.clearRect(0,0, canvas.width, canvas.height);
+        if (microphone.initialized){
+            ctx.clearRect(0,0, canvas.width, canvas.height);
+
+            const samples = microphone.getSamples();
         
-     
-        bars.forEach(function(bar){
-            bar.update(100);
-            bar.draw(ctx);
-        });
+            
+         
+            bars.forEach(function(bar,i){
+                bar.update(samples[i]);
+                bar.draw(ctx);
+            });
+
+        }
+   
         requestAnimationFrame(animate);
     
     }
